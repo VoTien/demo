8cf0e50e19d6ecc1954a5544d6f9650d17d3790e
VoTien <trieutienv@gmail.com>
2024-05-24 23:34:36 +0700

    Update HomeController.java

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -17,18 +17,29 @@ public class HomeController {
     public String returnCourse(){
         return "Blizzard";
     }
-    //@RequestMapping("/")
-    //public String home(){
-    //    System.out.println("home method called...");
-    //    return "index.jsp";
-    //}
     @RequestMapping("/")
-    public ModelAndView home(ModelAndView mv){
-        System.out.println("home method called...");
-        mv.addObject("msg", "index");
-        mv.setViewName("index.jsp");
-        return mv;
+    @ResponseBody
+    public String home(){
+        String html = "<html>\n" +
+                "    <head>\n" +
+                "        <link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">\n" +
+                "    </head>\n" +
+                "    </body>\n" +
+                "        <h2>Calculator</h2>\n" +
+                "\n" +
+                "         <form action=\"addAlien\">\n" +
+                "                <label for=\"aid\">Enter Id :</label>\n" +
+                "                <input type=\"text\" id=\"aid\" name=\"aid\"><br>\n" +
+                "                <label for=\"aname\">Enter Name :</label>\n" +
+                "                <input type=\"text\" id=\"aname\" name=\"aname\"><br>\n" +
+                "                <input type=\"submit\" value=\"Submit\">\n" +
+                "            </form>\n" +
+                "\n" +
+                "    </body>\n" +
+                "</html>";
+        return html;
     }
+
     @RequestMapping("add")
     //public String add(HttpServletRequest req, HttpSession session){
     //public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2 , HttpSession session){
