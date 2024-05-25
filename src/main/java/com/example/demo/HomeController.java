e6ab15cc22a735e7b75178b62c7973d37bccce4a
TienVo <trieutienv@gmail.com>
2024-05-25 17:51:09 +0700

    final

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -4,13 +4,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -58,6 +53,6 @@ public class HomeController {
 
     @RequestMapping("addAlien")
     public String addAlien(@ModelAttribute("alien1") Alien alien) {
-        return "result.jsp";
+        return "result";
     }
 }
